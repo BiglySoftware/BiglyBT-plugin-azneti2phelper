b285d2dabe3750b9fc4600722157e10762c2ca9a
parg <parg@users.noreply.github.com>
2022-12-01 15:11:30 +0000

    Might as well support multiple views

--- org/parg/azureus/plugins/networks/i2p/swt/I2PHelperView.java
+++ org/parg/azureus/plugins/networks/i2p/swt/I2PHelperView.java
@@ -26,7 +26,10 @@ package org.parg.azureus.plugins.networks.i2p.swt;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import com.biglybt.ui.mdi.MultipleDocumentInterface;
 import org.eclipse.swt.SWT;
@@ -82,20 +85,7 @@ I2PHelperView
 	private UISWTInstance		ui;
 	private String				view_id;
 	
-	private UISWTView			current_view;
-	
 
-	private Composite	composite;
-
-	private int					normal_dht_count;
-	private int					proxy_dht_count;
-	
-	private List<I2PHelperDHTPluginInterface> proxy_dhts;
-	
-	private DHTView[] 			dht_views;
-	private DHTOpsView[]		ops_views;
-	
-	private TimerEventPeriodic	view_timer;
 	private TimerEventPeriodic	status_timer;
 	
 	private UISWTStatusEntry	status_icon;
@@ -103,7 +93,8 @@ I2PHelperView
 	private Image				img_sb_enabled;
 	private Image				img_sb_disabled;
 	
-	
+	private Map<UISWTView,ViewInstance>		views = new HashMap<>();
+
 	private boolean		unloaded;
 	
 	public
@@ -289,181 +280,303 @@ I2PHelperView
 				
 		return( graphic );
 	}
-	
-	protected void
-	initialise(
-		Composite	_composite )
-	{
-		composite	= _composite;
 		
-		Composite main = new Composite(composite, SWT.NONE);
-		GridLayout layout = new GridLayout();
-		layout.numColumns = 1;
-		layout.marginHeight = 0;
-		layout.marginWidth = 0;
-		main.setLayout(layout);
-		GridData grid_data = new GridData(GridData.FILL_BOTH );
-		main.setLayoutData(grid_data);
-		
-		CTabFolder  tab_folder = new CTabFolder(main, SWT.LEFT);
-		tab_folder.setBorderVisible(true);
-		tab_folder.setTabHeight(20);
-		
-		Label lblClose = new Label(tab_folder, SWT.WRAP);
-		lblClose.setText("x");
-		lblClose.addListener(SWT.MouseUp, new Listener() {
-			@Override
-			public void handleEvent(Event event) {
-				
-			}
-		});
-		tab_folder.setTopRight(lblClose);
-		
-		grid_data = new GridData(GridData.FILL_BOTH);
-		tab_folder.setLayoutData(grid_data);
-		
-		CTabItem first_stats_item = null;
-		
-		normal_dht_count = plugin.getDHTCount();
-		
-		if ( Constants.isCVSVersion()){
-			
-			proxy_dhts = plugin.getProxyDHTs();
-			
-		}else{
-			
-			proxy_dhts = new ArrayList<I2PHelperDHTPluginInterface>();
-		}
-		
-		proxy_dht_count = proxy_dhts.size();
-		
-		int	total_dhts	= normal_dht_count+proxy_dht_count;
-		
-		int	total_views = total_dhts*2;		// basic + AZ views
-		
-		dht_views	= new DHTView[ total_views ];
-		ops_views	= new DHTOpsView[ total_views ];
 
-			// views are basic1 basic2 (proxy1,proxy2...) az1 az2 (proxyaz1, proxyaz2...)
-			// actually the proxy1/2/... views are dead as there is no non-AZ traffic
-		
-		for ( int i=0;i<total_views;i++){
-							
-			int	dht_index;
-			
-			if ( i < total_dhts ){
-				dht_index = i;
-			}else{
-				dht_index = i - total_dhts;
-			}
-			
-			String stats_text;
-			String graph_text;
-			
-			if ( dht_index < normal_dht_count ){
-				stats_text = plugin.getMessageText("azi2phelper.ui.dht_stats" + dht_index);
-				graph_text = plugin.getMessageText("azi2phelper.ui.dht_graph" + dht_index);
-			}else{
-				if ( i < total_dhts ){
-					continue;	// skip this view as dead
-				}
-				String name = proxy_dhts.get(dht_index - normal_dht_count).getName();
-				stats_text = name + " " + plugin.getMessageText("azi2phelper.ui.dht_stats3");
-				graph_text = name + " " + plugin.getMessageText("azi2phelper.ui.dht_graph3");
-			}
-			if ( i >= total_dhts ){
-				
-				stats_text = stats_text + " (AZ)";
-				graph_text = graph_text + " (AZ)";
-			}
-			
-				// DHT stats view
-				
-			CTabItem stats_item = new CTabItem(tab_folder, SWT.NULL);
-	
-			if ( i == 0 ){
-				
-				first_stats_item = stats_item;
-			}
-
-			stats_item.setText( stats_text );
-			
-			DHTView dht_view = dht_views[i] = new DHTView( false );
-			Composite stats_comp = new Composite( tab_folder, SWT.NULL );
-			stats_item.setControl( stats_comp );
-			layout = new GridLayout();
-			layout.numColumns = 1;
-			layout.marginHeight = 0;
-			layout.marginWidth = 0;
-			stats_comp.setLayout(layout);
-			grid_data = new GridData(GridData.FILL_BOTH);
-			stats_comp.setLayoutData(grid_data);
-			dht_view.initialize( stats_comp );
-				
-			fixupView( stats_comp );
-			
-				// DHT Graph view
-			
-			CTabItem ops_item = new CTabItem(tab_folder, SWT.NULL);
-	
-			ops_item.setText( graph_text );
-			
-			DHTOpsView ops_view = ops_views[i] = new DHTOpsView( false, false );
-			Composite ops_comp = new Composite( tab_folder, SWT.NULL );
-			ops_item.setControl( ops_comp );
-			layout = new GridLayout();
-			layout.numColumns = 1;
-			layout.marginHeight = 0;
-			layout.marginWidth = 0;
-			ops_comp.setLayout(layout);
-			grid_data = new GridData(GridData.FILL_BOTH);
-			ops_comp.setLayoutData(grid_data);
-			ops_view.initialize( ops_comp );
-				
-			fixupView( ops_comp );
-		}
-		
-		tab_folder.setSelection( first_stats_item );
-	}
-
-	private void
-	fixupView(
-		Composite	comp )
-	{
-			// hack to do the same as normal view construction code :(
-			
-		Control[] children = comp.getChildren();
-		for (int i = 0; i < children.length; i++) {
-			Control control = children[i];
-			Object layoutData = control.getLayoutData();
-			if (layoutData == null || !(layoutData instanceof GridData)) {
-				GridData grid_data;
-				if (children.length == 1)
-					grid_data = new GridData(SWT.FILL, SWT.FILL, true, true);
-				else
-					grid_data = new GridData();
-	
-				control.setLayoutData(grid_data);
-			}
-		}	
-	}
 	@Override
 	public boolean
 	eventOccurred(
 		UISWTViewEvent event )
 	{
+		UISWTView view = event.getView();
+		
+		ViewInstance instance;
+		
+		synchronized( views ){
+			
+			instance = views.get( view );
+		}
+		
 		switch( event.getType() ){
 
 			case UISWTViewEvent.TYPE_CREATE:{
 				
-				if ( current_view != null ){
+				if ( instance != null ){
 					
 					return( false );
 				}
 				
-				current_view = event.getView();
+				instance = new ViewInstance( plugin, view );
 				
-				view_timer = SimpleTimer.addPeriodicEvent(
+				synchronized( views ){
+					
+					if ( views.containsKey( view )){
+						
+						return( false );
+					}
+					
+					views.put( view, instance );
+				}
+				
+				break;
+			}
+			case UISWTViewEvent.TYPE_INITIALIZE:{
+				
+				if ( instance != null ){
+				
+					instance.initialise((Composite)event.getData());
+				}
+				
+				break;
+			}
+			case UISWTViewEvent.TYPE_REFRESH:{
+				
+				if ( instance != null ){
+					
+					instance.refresh( event );
+				}
+				
+				break;
+			}
+			case UISWTViewEvent.TYPE_DESTROY:{
+				
+				if ( instance != null ){
+					
+					instance.destroy();
+				}
+				
+				break;
+			}
+		}
+		
+		return true;
+	}
+	
+	public void
+	unload()
+	{
+		synchronized( this ){
+			
+			unloaded = true;
+		}
+		
+		ui.removeViews( UISWTInstance.VIEW_MAIN, view_id );
+		
+		List<ViewInstance>	to_destroy;
+		
+		synchronized( views ){
+			
+			to_destroy = new ArrayList<>( views.values());
+		}
+		
+		for ( ViewInstance view: to_destroy ){
+			
+			view.destroy();
+		}
+		
+		if ( status_timer != null ){
+			
+			status_timer.cancel();
+			
+			status_timer = null;
+		}
+		
+		if ( status_icon != null ){
+			
+			status_icon.destroy();
+			
+			status_icon = null;
+		}
+		
+		Utils.execSWTThread(
+			new Runnable()
+			{
+				@Override
+				public void
+				run()
+				{
+					if ( img_sb_disabled != null ){
+						
+						img_sb_disabled.dispose();
+						
+						img_sb_disabled = null;
+					}
+					
+					if ( img_sb_enabled != null ){
+						
+						img_sb_enabled.dispose();
+						
+						img_sb_enabled = null;
+					}
+				}
+			});
+		
+	}
+	
+	private static class
+	ViewInstance
+	{
+		private final I2PHelperPlugin		plugin;
+		private final UISWTView				view;
+		
+		private Composite	composite;
+
+		private int					normal_dht_count;
+		private int					proxy_dht_count;
+		
+		private List<I2PHelperDHTPluginInterface> proxy_dhts;
+		
+		private DHTView[] 			dht_views;
+		private DHTOpsView[]		ops_views;
+		
+		private TimerEventPeriodic	view_timer;
+		
+		AtomicBoolean	destroyed = new AtomicBoolean();
+		
+		private
+		ViewInstance(
+			I2PHelperPlugin		_plugin,
+			UISWTView			_view )
+		{	
+			plugin	= _plugin;
+			view	= _view;
+		}
+		
+		protected void
+		initialise(
+			Composite	_composite )
+		{
+			composite	= _composite;
+			
+			Composite main = new Composite(composite, SWT.NONE);
+			GridLayout layout = new GridLayout();
+			layout.numColumns = 1;
+			layout.marginHeight = 0;
+			layout.marginWidth = 0;
+			main.setLayout(layout);
+			GridData grid_data = new GridData(GridData.FILL_BOTH );
+			main.setLayoutData(grid_data);
+			
+			CTabFolder  tab_folder = new CTabFolder(main, SWT.LEFT);
+			tab_folder.setBorderVisible(true);
+			tab_folder.setTabHeight(20);
+			
+			Label lblClose = new Label(tab_folder, SWT.WRAP);
+			lblClose.setText("x");
+			lblClose.addListener(SWT.MouseUp, new Listener() {
+				@Override
+				public void handleEvent(Event event) {
+					
+				}
+			});
+			tab_folder.setTopRight(lblClose);
+			
+			grid_data = new GridData(GridData.FILL_BOTH);
+			tab_folder.setLayoutData(grid_data);
+			
+			CTabItem first_stats_item = null;
+			
+			normal_dht_count = plugin.getDHTCount();
+			
+			if ( Constants.isCVSVersion()){
+				
+				proxy_dhts = plugin.getProxyDHTs();
+				
+			}else{
+				
+				proxy_dhts = new ArrayList<I2PHelperDHTPluginInterface>();
+			}
+			
+			proxy_dht_count = proxy_dhts.size();
+			
+			int	total_dhts	= normal_dht_count+proxy_dht_count;
+			
+			int	total_views = total_dhts*2;		// basic + AZ views
+			
+			dht_views	= new DHTView[ total_views ];
+			ops_views	= new DHTOpsView[ total_views ];
+
+				// views are basic1 basic2 (proxy1,proxy2...) az1 az2 (proxyaz1, proxyaz2...)
+				// actually the proxy1/2/... views are dead as there is no non-AZ traffic
+			
+			for ( int i=0;i<total_views;i++){
+								
+				int	dht_index;
+				
+				if ( i < total_dhts ){
+					dht_index = i;
+				}else{
+					dht_index = i - total_dhts;
+				}
+				
+				String stats_text;
+				String graph_text;
+				
+				if ( dht_index < normal_dht_count ){
+					stats_text = plugin.getMessageText("azi2phelper.ui.dht_stats" + dht_index);
+					graph_text = plugin.getMessageText("azi2phelper.ui.dht_graph" + dht_index);
+				}else{
+					if ( i < total_dhts ){
+						continue;	// skip this view as dead
+					}
+					String name = proxy_dhts.get(dht_index - normal_dht_count).getName();
+					stats_text = name + " " + plugin.getMessageText("azi2phelper.ui.dht_stats3");
+					graph_text = name + " " + plugin.getMessageText("azi2phelper.ui.dht_graph3");
+				}
+				if ( i >= total_dhts ){
+					
+					stats_text = stats_text + " (AZ)";
+					graph_text = graph_text + " (AZ)";
+				}
+				
+					// DHT stats view
+					
+				CTabItem stats_item = new CTabItem(tab_folder, SWT.NULL);
+		
+				if ( i == 0 ){
+					
+					first_stats_item = stats_item;
+				}
+
+				stats_item.setText( stats_text );
+				
+				DHTView dht_view = dht_views[i] = new DHTView( false );
+				Composite stats_comp = new Composite( tab_folder, SWT.NULL );
+				stats_item.setControl( stats_comp );
+				layout = new GridLayout();
+				layout.numColumns = 1;
+				layout.marginHeight = 0;
+				layout.marginWidth = 0;
+				stats_comp.setLayout(layout);
+				grid_data = new GridData(GridData.FILL_BOTH);
+				stats_comp.setLayoutData(grid_data);
+				dht_view.initialize( stats_comp );
+					
+				fixupView( stats_comp );
+				
+					// DHT Graph view
+				
+				CTabItem ops_item = new CTabItem(tab_folder, SWT.NULL);
+		
+				ops_item.setText( graph_text );
+				
+				DHTOpsView ops_view = ops_views[i] = new DHTOpsView( false, false );
+				Composite ops_comp = new Composite( tab_folder, SWT.NULL );
+				ops_item.setControl( ops_comp );
+				layout = new GridLayout();
+				layout.numColumns = 1;
+				layout.marginHeight = 0;
+				layout.marginWidth = 0;
+				ops_comp.setLayout(layout);
+				grid_data = new GridData(GridData.FILL_BOTH);
+				ops_comp.setLayoutData(grid_data);
+				ops_view.initialize( ops_comp );
+					
+				fixupView( ops_comp );
+			}
+			
+			tab_folder.setSelection( first_stats_item );
+			
+			view_timer = SimpleTimer.addPeriodicEvent(
 					"I2PView:stats",
 					1000,
 					new TimerEventPerformer() 
@@ -512,199 +625,160 @@ I2PHelperView
 							}
 						}
 					});
-				
-				break;
-			}
-			case UISWTViewEvent.TYPE_INITIALIZE:{
-				
-				initialise((Composite)event.getData());
-				
-				break;
-			}
-			case UISWTViewEvent.TYPE_REFRESH:{
-				
-				if ( dht_views != null ){
-				
-					int	total_dhts	= normal_dht_count+proxy_dht_count;
+		}
 
-					for ( int i=0;i<dht_views.length;i++){
+		private void
+		fixupView(
+			Composite	comp )
+		{
+				// hack to do the same as normal view construction code :(
+				
+			Control[] children = comp.getChildren();
+			for (int i = 0; i < children.length; i++) {
+				Control control = children[i];
+				Object layoutData = control.getLayoutData();
+				if (layoutData == null || !(layoutData instanceof GridData)) {
+					GridData grid_data;
+					if (children.length == 1)
+						grid_data = new GridData(SWT.FILL, SWT.FILL, true, true);
+					else
+						grid_data = new GridData();
+		
+					control.setLayoutData(grid_data);
+				}
+			}	
+		}
+		
+		private void
+		refresh(
+			UISWTViewEvent		event )
+		{
+			if ( dht_views != null ){
+				
+				int	total_dhts	= normal_dht_count+proxy_dht_count;
+
+				for ( int i=0;i<dht_views.length;i++){
+					
+					DHTView 	dht_view = dht_views[i];
+					DHTOpsView 	ops_view = ops_views[i];
+					
+					if ( dht_view != null && ops_view != null ){
 						
-						DHTView 	dht_view = dht_views[i];
-						DHTOpsView 	ops_view = ops_views[i];
+						int	dht_index;
 						
-						if ( dht_view != null && ops_view != null ){
+						if ( i < total_dhts ){
 							
-							int	dht_index;
+							dht_index = i;
 							
-							if ( i < total_dhts ){
-								
-								dht_index = i;
-								
-							}else{
-								
-								dht_index = i - total_dhts;
-							}
+						}else{
 							
-							if ( dht_index < normal_dht_count ){
-								
-								I2PHelperRouter router = plugin.getRouter();
-								
-								if ( router != null ){
-										
-									I2PHelperDHT dht_helper = router.getAllDHTs()[dht_index].getDHT();
+							dht_index = i - total_dhts;
+						}
+						
+						if ( dht_index < normal_dht_count ){
+							
+							I2PHelperRouter router = plugin.getRouter();
+							
+							if ( router != null ){
 									
-									if ( dht_helper instanceof DHTI2P ){
-										
-										DHT dht;
-										
-										if ( i < normal_dht_count ){
-											
-											dht = ((DHTI2P)dht_helper).getDHT();
-										}else{
-											
-											dht = ((DHTI2P)dht_helper).getAZDHT().getDHT();
-										}
-										
-										dht_view.setDHT( dht );
-										ops_view.setDHT( dht );
-									}
-								}
-							}else{
+								I2PHelperDHT dht_helper = router.getAllDHTs()[dht_index].getDHT();
 								
-								I2PHelperDHTPluginInterface proxy_dht = proxy_dhts.get( dht_index - normal_dht_count);
-								
-								I2PHelperAZDHT dht_helper = proxy_dht.getDHT( 1 );
-								
-								if ( dht_helper != null ){
+								if ( dht_helper instanceof DHTI2P ){
 									
 									DHT dht;
 									
-									try{
-										if ( i < normal_dht_count + proxy_dht_count ){
-											
-											dht = dht_helper.getBaseDHT();
-										}else{
-											
-											dht = dht_helper.getDHT();
-										}
+									if ( i < normal_dht_count ){
 										
-										dht_view.setDHT( dht );
-										ops_view.setDHT( dht );
+										dht = ((DHTI2P)dht_helper).getDHT();
+									}else{
 										
-									}catch( Throwable e ){
-										
+										dht = ((DHTI2P)dht_helper).getAZDHT().getDHT();
 									}
+									
+									dht_view.setDHT( dht );
+									ops_view.setDHT( dht );
 								}
 							}
+						}else{
 							
-							dht_view.eventOccurred(event);
-							ops_view.eventOccurred(event);
-						}
-					}
-				}
-				
-				break;
-			}
-			case UISWTViewEvent.TYPE_DESTROY:{
-				
-				try{
-					composite = null;
-					
-					if ( view_timer != null ){
-						
-						view_timer.cancel();
-						
-						view_timer = null;
-					}
-					
-					if ( dht_views != null ){
-						
-						for ( int i=0;i<dht_views.length;i++){
+							I2PHelperDHTPluginInterface proxy_dht = proxy_dhts.get( dht_index - normal_dht_count);
 							
-							DHTView 	dht_view = dht_views[i];
+							I2PHelperAZDHT dht_helper = proxy_dht.getDHT( 1 );
 							
-							if ( dht_view != null ){
+							if ( dht_helper != null ){
 								
-								dht_view.delete();
+								DHT dht;
 								
-								dht_views[i] = null;
-							}
-							
-							DHTOpsView 	ops_view = ops_views[i];
-							
-							if ( ops_view != null ){
-								
-								ops_view.delete();
-								
-								ops_views[i] = null;
+								try{
+									if ( i < normal_dht_count + proxy_dht_count ){
+										
+										dht = dht_helper.getBaseDHT();
+									}else{
+										
+										dht = dht_helper.getDHT();
+									}
+									
+									dht_view.setDHT( dht );
+									ops_view.setDHT( dht );
+									
+								}catch( Throwable e ){
+									
+								}
 							}
 						}
-					}
-				}finally{
-					
-					UISWTView view = current_view;
-					
-					if ( view != null ){
 						
-						current_view = null;
-						
-						view.closeView();
+						dht_view.eventOccurred(event);
+						ops_view.eventOccurred(event);
 					}
 				}
-				
-				break;
 			}
 		}
 		
-		return true;
-	}
-	
-	public void
-	unload()
-	{
-		synchronized( this ){
+		private void
+		destroy()
+		{
+			if ( !destroyed.compareAndSet( false, true )){
+				
+				return;
+			}
 			
-			unloaded = true;
-		}
-		
-		ui.removeViews( UISWTInstance.VIEW_MAIN, view_id );
-		
-		if ( status_timer != null ){
-			
-			status_timer.cancel();
-			
-			status_timer = null;
-		}
-		
-		if ( status_icon != null ){
-			
-			status_icon.destroy();
-			
-			status_icon = null;
-		}
-		
-		Utils.execSWTThread(
-			new Runnable()
-			{
-				@Override
-				public void
-				run()
-				{
-					if ( img_sb_disabled != null ){
-						
-						img_sb_disabled.dispose();
-						
-						img_sb_disabled = null;
-					}
+			try{
+				composite = null;
+				
+				if ( view_timer != null ){
 					
-					if ( img_sb_enabled != null ){
+					view_timer.cancel();
+					
+					view_timer = null;
+				}
+				
+				if ( dht_views != null ){
+					
+					for ( int i=0;i<dht_views.length;i++){
 						
-						img_sb_enabled.dispose();
+						DHTView 	dht_view = dht_views[i];
 						
-						img_sb_enabled = null;
+						if ( dht_view != null ){
+							
+							dht_view.delete();
+							
+							dht_views[i] = null;
+						}
+						
+						DHTOpsView 	ops_view = ops_views[i];
+						
+						if ( ops_view != null ){
+							
+							ops_view.delete();
+							
+							ops_views[i] = null;
+						}
 					}
 				}
-			});
-		
+			}finally{
+									
+				view.closeView();
+			}
+		}
 	}
 }
